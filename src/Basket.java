75e854646e97541ace76b34b7027fa2cb234f35a
S <q20142015@yandex.ru>
2023-03-21 11:28:14 +0300

    Исправлен add

---
+++ src/Basket.java
@@ -0,0 +1,93 @@
+public class Basket {
+
+    private static int count = 0;
+    private String items = "";
+    private int totalPrice = 0;
+    private int limit;
+
+   private double  totalWeight=0;
+    public Basket() {
+        increaseCount(1);
+        items = "Список товаров:";
+        this.limit = 1000000;
+    }
+
+    public void add(String name, int price, int count, double weight)    {
+        boolean error=add(name, price, count);
+        if (error) {
+            return;
+        }
+        totalWeight=totalWeight+weight;
+    }
+    public double getTotalWeight()
+    {
+        return totalWeight;
+    }
+    public Basket(int limit) {
+        this();
+        this.limit = limit;
+    }
+
+    public Basket(String items, int totalPrice) {
+        this();
+        this.items = this.items + items;
+        this.totalPrice = totalPrice;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void increaseCount(int count) {
+        Basket.count = Basket.count + count;
+    }
+
+    public void add(String name, int price) {
+        add(name, price, 1);
+    }
+
+    public boolean add(String name, int price, int count) {
+        boolean error = false;
+        boolean test=contains(name);
+        if (contains(name)) {
+            error = true;
+        }
+
+        if (totalPrice + count * price >= limit) {
+            error = true;
+        }
+
+        if (error) {
+            System.out.println("Error occured :(");
+            return error;
+        }
+
+        items = items + "\n" + name + " - " +
+            count + " шт. - " + price;
+        totalPrice = totalPrice + count * price;
+        return error;
+    }
+
+    public void clear() {
+        items = "";
+        totalPrice = 0;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public boolean contains(String name) {
+
+        return items.contains(name);
+    }
+
+    public void print(String title) {
+        System.out.println(title);
+        if (items.isEmpty()) {
+            System.out.println("Корзина пуста");
+        } else {
+            System.out.println(items);
+        }
+    }
+}
